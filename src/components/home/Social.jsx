2e96e572f19fb8502c181454773d969c871327fe
Balaji Mohan <95994255+BalajiMkv@users.noreply.github.com>
2023-02-05 20:45:23 +0530

    updated scrolldown component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a
+        href="https://www.instagram.com/"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
